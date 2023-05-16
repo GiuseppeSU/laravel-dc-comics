a1f3fa160cb8c8cc89510fd38e43fd00e291d64a
GiuseppeSuanno <suanno.giuseppe@yahoo.it>
2023-05-16 16:19:51 +0200

    Commit di aggiornamento

--- database/migrations/2023_05_16_132321_create_comics_table.php
+++ database/migrations/2023_05_16_132321_create_comics_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,6 +14,14 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->longText('description');
+            $table->string('thumb');
+            $table->float('price', 4, 2);
+            $table->string('series');
+            $table->string('sale_date');
+            $table->string('type');
+
             $table->timestamps();
         });
     }
